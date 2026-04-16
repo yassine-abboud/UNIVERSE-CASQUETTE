c5b114711912bdd8fed195c83ef815e3047cf00b
yassine-abboud <abboudyassine47@gmail.com>
2026-04-16 21:06:39 +0100

    Add files via upload

---
+++ hero.js
@@ -0,0 +1,24 @@
+  tl.from('.container', {
+opacity: 0,
+duration:2.5,
+y:-70,
+ease:"power2.inOut",
+})
+tl.from('h1', {
+opacity: 0,
+duration:2.5,
+y:-70,
+ease:"power2.inOut",
+})
+
+tl.from('p', {
+opacity: 0,
+duration:1.2,
+scale:2,
+})
+tl.from('.btn', {
+opacity: 0,
+duration:1.5,
+x:-70,
+ease:"power2.inOut",
+})
